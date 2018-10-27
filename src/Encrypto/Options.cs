dd482024aed71a68bd4099221872238a1991fa94
tomchavakis <tomchavakis@gmail.com>
2018-10-27 15:13:32 +0300

    adding usage attribute

--- src/Encrypto/Options.cs
+++ src/Encrypto/Options.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using CommandLine;
+using CommandLine.Text;
 
 namespace Encrypto
 {
@@ -12,6 +14,20 @@ namespace Encrypto
 
         [Option('t', "text", HelpText = "Insert the text for encryption")]
         public string InputText { get; set; }
+
+        [Usage(ApplicationAlias = "dotnet-encrypto")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                new Example("encrypt folder", new EncryptOptions { InputFile = "~/Downloads/test" }),
+                new Example("encrypt file", new EncryptOptions { InputFile = "~/Downloads/text.txt" }),
+                new Example("encrypt text", new EncryptOptions { InputText = "thomas" }),
+                new Example("encrypt text with escape characters", new EncryptOptions { InputText = "text \"to encrypt" }),
+            };
+            }
+        }
     }
 
     [Verb("decrypt", HelpText = "Decrypt File(s) Or Folder(s")]
@@ -23,6 +39,21 @@ namespace Encrypto
 
         [Option('t', "text", HelpText = "Insert the text for decryption")]
         public string InputText { get; set; }
+
+
+        [Usage(ApplicationAlias = "dotnet-encrypto")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                new Example("encrypt folder", new DecryptOptions { InputFile = "~/Downloads/test" }),
+                new Example("encrypt file", new DecryptOptions { InputFile = "~/Downloads/text.txt" }),
+                new Example("encrypt text", new DecryptOptions { InputText = "thomas" }),
+                new Example("encrypt text with escape characters", new DecryptOptions { InputText = "text \"to encrypt" }),
+            };
+            }
+        }
 
     }
 
